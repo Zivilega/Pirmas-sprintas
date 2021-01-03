41e0d799a1ba5642d282f5d27ed315cd21c5530c
Zivilega <72792721+Zivilega@users.noreply.github.com>
2021-01-03 12:27:50 +0200

    Failu ikelimas

---
+++ incl/footer.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+list($seconds, $microseconds) = explode(" ", microtime());
+$time_end = $seconds + $microseconds;
+$total_time = round($time_end-$time_start, 4);
+
+print "<br /><br />";
+print "<table class='bottom' cellpadding=0 cellspacing=0>";
+  print "<tr><td align='center'>Zivile Bataite: SPRINT NR 1</td></tr>";
+  print "<tr><td>&nbsp;</td></tr>";
+print "</table>";
+
+print "<br /><br />";
+
+print "</center>";
+print "</body>";
+print "</html>";
---
+++ lang/lithuanian.inc.php
@@ -0,0 +1,125 @@
+<?php
+
+$StrLanguageCharset             = "ISO-8859-1";
+
+$StrIndexOf                     = "Indeksas";
+
+$StrMenuCreateFolder            = "Sukurti nauja kataloga";
+$StrMenuCreateFile              = "Sukurti nauja faila";
+$StrMenuUploadFiles             = "Ikelti faila";
+$StrMenuLogOut                  = "Atsijungti";
+
+$StrOpenFolder                  = "Atidaryti kataloga";
+$StrRenameFolder                = "Pervadinti kataloga";
+$StrDeleteFolder                = "Istrinti kataloa";
+
+$StrViewFile                    = "Rodyti faila";
+$StrEditFile                    = "Koreguoti faila";
+$StrRenameFile                  = "Pervadinti faila";
+$StrDownloadFile                = "Ikelti faila";
+$StrDeleteFile                  = "Istrinti faila";
+
+$StrFile                        = "Failas";
+
+$StrFolderNameShort             = "Pavadinimas";
+$StrFileNameShort               = "Pavadinimas";
+$StrFileSizeShort               = "Dydis";
+$StrPermissionsShort            = "L";
+$StrLastModifiedShort           = "Modifikuotas";
+$StrEditShort                   = "Red";
+$StrViewShort                   = "Zr";
+$StrRenameShort                 = "Perv";
+$StrDownloadShort               = "S";
+$StrDeleteShort                 = "Tr";
+
+$StrBack                        = "Atgal";
+$StrYes                         = "Taip";
+$StrOr                          = "arba";
+$StrCancel                      = "Atsaukti";
+
+$StrUsername                    = "Prisijungimo vardas";
+$StrPassword                    = "Slaptazodis:";
+$StrLogIn                       = "Prisijungti";
+$StrLoginSystem                 = "Prisijungimo sistema:";
+$StrLoginInfo                   = "Prasome ivesto prisijungimo varda ir slaptazodi;:";
+
+$StrAccessDenied                = "Prieiga uzdrausta!";
+
+$StrInvalidHomeFolder           = "Neteisingas namu katalogas.";
+$StrInvalidPath                 = "Neteisingas kelias.";
+$StrMissingTrailingSlash        = "(Trūksta pasvirojo brūksnio)";
+$StrAlreadyExists               = "Failas arba katalogas su tokiu pavadinimu jau egzistuoja.";
+$StrFolderInvalidName           = "Neteisingas katalogo pavadinimas.";
+$StrFileInvalidName             = "Neteisingas failo pavadinimas.";
+$StrErrorOpeningFile            = "Klaida atidarant faila.";
+
+$StrSaveFileSuccess             = "Failas issaugotas sekmingai!";
+$StrSaveFileFail                = "Nepavyko issaugoti failo.";
+$StrEditing                     = "Redagavimas";
+$StrFilename                    = "Failo pavadinimas:";
+$StrRestoreOriginal             = "Atstatyti originalu";
+$StrSaveAndExit                 = "Issaugoti & iseiti";
+
+$StrDeleteFolderNotFound        = "Katalogas nerastas.";
+$StrDeleteFileNotFound          = "failas nerastas.";
+$StrDeleteFolderSuccess         = "Katalogas istrintas sekmingai!";
+$StrDeleteFileSuccess           = "Failas istrintas sekmingai!";
+$StrDeleteFolderFail            = "Katalogo trynimas nepavyko.";
+$StrDeleteFileFail              = "Failo trynimas nepavyko.";
+$StrDeleteFolderFailHelp        = "This might be caused by insufficient permissions.";
+$StrDeleteFileFailHelp          = "This might be caused by insufficient permissions.";
+$StrDeleteFolderQuestion        = "Ar tikrai norite istrinti sekanti kataloga?";
+$StrDeleteFileQuestion          = "Ar tirkai norite istrinti sekanti faila?";
+
+$StrRename                      = "Pervadinti";
+$StrRenameFolder                = "Pervadinti kataloga";
+$StrRenameFile                  = "Pervadinti faila";
+$StrRenameFolderSuccess         = "Katalogas pervadintas sekmingai!";
+$StrRenameFileSuccess           = "Failas pervadintas sekmingai!";
+$StrRenameFolderFail            = "Nepavyko pervadinti katalogo.";
+$StrRenameFileFail              = "Nepavyko pervadinti failo.";
+$StrRenameFolderFailHelp        = "This might be caused by insufficient permissions or an invalid name of the folder.";
+$StrRenameFileFailHelp          = "This might be caused by insufficient permissions or an invalid filename.";
+$StrRenameFolderQuestion        = "Prasome pasirinkti nauja pavadinima sekanciam katalogui:";
+$StrRenameFileQuestion          = "Prasome pasirinkti neuja pavadinia sekanciam failui:";
+
+$StrCreate                      = "Sukurti";
+$StrCreateFolder                = "Sukurti nauja kataloga";
+$StrCreateFile                  = "Sukurti nauja faila";
+$StrCreateFolderSuccess         = "Katalogas sukurtas sekmingai!";
+$StrCreateFileSuccess           = "Failas sukurtas sekmingai!";
+$StrCreateFolderFail            = "Nepavyko sukurti katalogo.";
+$StrCreateFileFail              = "Nepavyko sukurti failo.";
+$StrCreateFolderFailHelp        = "This might be caused by insufficient permissions.";
+$StrCreateFileFailHelp          = "This might be caused by insufficient permissions.";
+$StrCreateFolderQuestion        = "Prasome pasirinkti nauja pavadinima katalogui";
+$StrCreateFileQuestion          = "Prasome pasirinkti nauja pavadinima failui:";
+
+$StrUpload                      = "Ikelti";
+$StrUploadFilesTo               = "Ikelti failus";
+$StrUploading                   = "Ikeliama";
+$StrUploadSuccess               = "Gerai!";
+$StrUploadFail                  = "NEPAVYKO!";
+$StrUploadFailPost              = "Pasiektas ikelimu maximumas - see docs/faq.txt for more information.";
+$StrFirstFile                   = "pirmas failas:";
+$StrSecondFile                  = "antras failas:";
+$StrThirdFile                   = "trecias failas:";
+$StrFourthFile                  = "ketvirtas failas:";
+$StrUploadQuestion              = "Pasirinkite failus kuriuos norite ikelti:";
+$StrUploadNote                  = "Pastaba: Ikelti failai bus ideti:";
+
+$StrDownload                    = "Parsisiusti";
+$StrDownloadClickLink           = "Paspausti nuoroda zemiau, kad pradeti siuntima.";
+$StrDownloadClickHere           = "Paspausti cia siuntimui";
+$StrDownloadFail                = "Klaida atidarant faila arba neteisingas failo pavadinimas. ";
+
+$StrViewing                     = "Ziurejimas";
+$StrAt                          = "at";
+$StrViewFail                    = "Klaida atidarant vaizda�.";
+$StrViewFailHelp                = "Failas neegzistuoja arba ne vaizdo failas.";
+$StrImage                       = "Vaizdas";
+$StrZoomIn                      = "Priartinimas";
+$StrZoomOut                     = "Nutolinimas";
+$StrOriginalSize                = "Originalus dydis";
+$StrPrevious                    = "Ankstesnis";
+$StrNext                        = "Sekantis";
